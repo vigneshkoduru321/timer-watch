0259f13b30ccd652b734c5e16314299c0d94f5d2
Vignesh Koduru <vigneshkoduru321@gmail.com>
2024-02-22 17:00:33 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css"
+
+class Stopwatch extends Component {
+  state={inputValue:null,timer:null,timerValue:null,isTimerSet: false,actualTimerInSec:null}
+  
+  startTimer=()=>{
+    this.intervalId = setInterval(() => {
+      this.setState(prevState => ({
+        actualTimerInSec: prevState.actualTimerInSec - 1
+      }));
+    }, 1000);
+  }
+
+
+  playAlarm = () => {
+    this.url = "http://streaming.tdiradio.com:8000/house.mp3";
+    this.audio = new Audio(this.url);
+    this.audio.play()
+    clearInterval(this.intervalId)
+  };
+
+  stopAudio(){
+    window.location.reload()
+  }
+
+  onChangeInput=(event) => {
+    this.setState({timer:event.target.value})
+    this.setState({inputValue:event.target.value})
+
+  }
+  reSetTimer=()=>{
+    window.location.reload()
+  }
+  setTimer =()=>{
+    const {timer}=this.state
+    this.setState({timerValue:timer*15})
+    this.setState({actualTimerInSec:timer*15})
+    this.setState({inputValue:""})
+    this.setState({isTimerSet:true})
+  }
+ 
+
+  render() {
+    const {timer,timerValue,inputValue,isTimerSet,actualTimerInSec}= this.state
+    if(actualTimerInSec===0){
+      this.playAlarm();
+    }
+    return (
+      <div className="main-background">
+       <div className='alarm-container'>
+        <h1>ALARM</h1>
+          <div>
+            <p>Set Timer in Minutes </p>
+              <div>
+              <input type="number" value={inputValue} onChange={this.onChangeInput}  placeholder='Enter ' className="input-timer"/>
+              <button onClick={this.setTimer} className='set-button'>SET</button>
+              <button onClick={this.reSetTimer} className='set-button'>RESET</button>
+              </div>
+              <div>
+                {isTimerSet && <p>Timer has been set for {timerValue/60} Minutes.</p>}
+                {isTimerSet && <button onClick={this.startTimer}>Start Timer</button>}
+                {isTimerSet && <h1>Remaining Time {actualTimerInSec} Sec</h1>}
+                {isTimerSet && <button onClick={this.stopAudio}>Stop audio</button>}
+              </div>
+          </div>
+       </div>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default Stopwatch;
